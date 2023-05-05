6ed4a450a6cdd5227145ba119c86ae509e512fd9
guilhermesantos0 <guilhermesantosdev@outlook.com>
2023-05-05 10:42:53 -0300

    Movimentar entre os quadrados

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ screen = pygame.display.set_mode((550,545))
 running = True
 
 pygame.display.set_caption("PyJa Cross", "https://cdn-icons-png.flaticon.com/512/124/124034.png?w=360")
+# icon = pygame.image.load("logo.png")
+# pygame.display.set_icon(icon)
 
 font = pygame.font.SysFont(None, 50)
 
 squareSide = 60
-
-drawed = False
 
 words = [
     "pygame",
@@ -44,12 +44,15 @@ blocks = [
     [{ "enabled": False, "letter": "" , "written": "" },{ "enabled": False, "letter": "" , "written": "" },{ "enabled": False, "letter": "" , "written": "" },{ "enabled": True, "letter": "e", "written": "" },{ "enabled": False, "letter": "" , "written": "" },{ "enabled": False, "letter": "" , "written": "" },{ "enabled": False, "letter": "" , "written": "" },{ "enabled": False, "letter": "" , "written": "" },{ "enabled": False, "letter": "" , "written": "" },{ "enabled": False, "letter": "" , "written": "" },{ "enabled": False, "letter": "" , "written": "" },]
 ]
 
-def drawSheet():
+def drawSheet(selected):
     for i in range(len(blocks)):
         for j in range(len(blocks[i])):
             if blocks[i][j]["enabled"]:
                 # print(i,j)
-                pygame.draw.rect(screen, colors["white"], (j * 50, i * 50, squareSide, squareSide))
+                if (j,i) == selected:
+                    pygame.draw.rect(screen, colors["red"], (j * 50, i * 50, squareSide, squareSide))
+                else:
+                    pygame.draw.rect(screen, colors["white"], (j * 50, i * 50, squareSide, squareSide))
 
             else:
                 pygame.draw.rect(screen, colors["gray"], (j * 50, i * 50, squareSide, squareSide))
@@ -59,20 +62,84 @@ def drawSheet():
             letter = font.render(blocks[i][j]["written"], True, colors["red"])
             screen.blit(letter, (j * 50 + 167, i * 50 + 117))
 
-def updateGrid():
-    drawSheet()
+def updateGrid(selected):
+    drawSheet(selected)
     pygame.display.update()
+
+def updateSelected(selected, direction):
+    x,y = selected or (0,0)
+
+    if direction == "down":
+
+        i = y
+        while True:
+            if i == len(blocks) - 1:
+                i = 0
+            else:
+                i += 1
+
+            if blocks[i][x]["enabled"]:
+                y = i
+                break
+
+    elif direction == "up":
+        i = y
+        while True:
+            if i == 0:
+                i = len(blocks) - 1
+            else:
+                i -= 1
+
+            if blocks[i][x]["enabled"]:
+                y = i
+                break
+
+    elif direction == "right":
+        i = x
+        while True:
+            if i == len(blocks[y]) - 1:
+                i = 0
+            else:
+                i += 1
+
+            if blocks[y][i]["enabled"]:
+                x = i
+                break
+
+    elif direction == "left":
+        i = x
+        while True:
+            if i == 0 :
+                i = len(blocks[y]) - 1
+            else:
+                i -= 1
+
+            if blocks[y][i]["enabled"]:
+                x = i
+                break
+
+    return (x,y)
+
+selected = (3,0)
 
 while running:
 
     pygame.time.delay(100)
 
-    screen.fill(colors["red"])
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    updateGrid()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_DOWN]:
+        selected = updateSelected(selected, "down")
+    if keys[pygame.K_UP]:
+        selected = updateSelected(selected, "up")
+    if keys[pygame.K_RIGHT]:
+        selected = updateSelected(selected, "right")
+    if keys[pygame.K_LEFT]:
+        selected = updateSelected(selected, "left")
+
+    updateGrid(selected)
 
 pygame.quit()
